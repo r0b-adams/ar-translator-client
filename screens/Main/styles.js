737b1bc209ffef6a860f2baac365c59e50395c3b
Robert Adams <adamsiii.robert@gmail.com>
2022-08-29 15:41:36 -0700

    Move stylesheet to own file

---
+++ screens/Main/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    bottom: 10,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+export default styles;
