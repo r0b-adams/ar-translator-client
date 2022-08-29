a4b929ebb8005355f5b228dd6acaa35daede57ed
Robert Adams <adamsiii.robert@gmail.com>
2022-08-29 15:47:28 -0700

    Add Main component to handle camera interface

---
+++ screens/Main/index.js
@@ -0,0 +1,77 @@
+import { useState, useRef } from 'react';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+
+import api from '../../utils/api';
+import styles from './styles';
+
+const LANG_TO = 'en';
+
+const Main = () => {
+  const [cameraReady, setCameraReady] = useState(false);
+  const [cameraType, setCameraType] = useState(CameraType.back);
+  const [permission, grantPermission] = Camera.useCameraPermissions();
+  const cam = useRef(null);
+
+  // Camera permissions are still loading
+  if (!permission) {
+    return <View />;
+  }
+
+  // Camera permissions are not granted yet
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={grantPermission} title='grant permission' />
+      </View>
+    );
+  }
+
+  const toggleCameraType = () => {
+    setCameraType((type) =>
+      type === CameraType.back ? CameraType.front : CameraType.back
+    );
+  };
+
+  const analyze = async () => {
+    try {
+      if (!cameraReady) {
+        throw new Error('Failed to take photo; Camera failed to mount');
+      }
+
+      const { base64 } = await cam.current.takePictureAsync({ base64: true });
+      const { data } = await api.analyze(base64, LANG_TO);
+
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Camera
+        ref={cam}
+        style={styles.camera}
+        type={cameraType}
+        ratio='16:9'
+        onCameraReady={() => setCameraReady(() => true)}
+        onMountError={() => console.log('CAMERA MOUNT ERROR')}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={analyze}>
+            <Text style={styles.text}>Take Photo</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+};
+
+export default Main;
