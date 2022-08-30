ce1a0d399882e143ef9190511de3e9fee828e524
Robert Adams <adamsiii.robert@gmail.com>
2022-08-30 11:19:01 -0700

    Update api helper with req aliases for endpoints

--- utils/api/index.js
+++ utils/api/index.js
@@ -6,12 +6,32 @@ axios.interceptors.request.use(setAuth, handleError);
 axios.interceptors.request.use(setHost, handleError);
 
 const api = {
+  user: async () => {
+    return axios.get('/auth/users');
+  },
+
+  register: async (username, email, password) => {
+    return axios.post('/auth/register', { username, email, password });
+  },
+
+  login: async (username, password) => {
+    return axios.post('/auth/login', { username, password });
+  },
+
+  logout: async () => {
+    return axios.delete('/auth/logout');
+  },
+
+  languages: async () => {
+    return axios.get('/translateAPI/languages');
+  },
+
+  translate: async (text, from, to) => {
+    return axios.post('/translateAPI/translate', { text, from, to });
+  },
+
   analyze: async (img, to) => {
-    return axios({
-      method: 'post',
-      url: '/visionAPI/objects',
-      data: { img, to },
-    });
+    return axios.post('/visionAPI/objects', { img, to });
   },
 };
 
