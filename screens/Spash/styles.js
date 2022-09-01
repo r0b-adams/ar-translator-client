89f6b356d46a781903fc140c38554710d3d86398
Robert Adams <adamsiii.robert@gmail.com>
2022-08-31 20:50:24 -0700

    Add basic screen stylesheets

---
+++ screens/Spash/styles.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  screen: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+});
+
+export default styles;
