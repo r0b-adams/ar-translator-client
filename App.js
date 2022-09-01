69b1a9f7d9b05b858b1973bffcd30ab63ff328cb
Robert Adams <adamsiii.robert@gmail.com>
2022-08-31 21:01:29 -0700

    Add routes, add state with useReducer, add context

--- App.js
+++ App.js
@@ -1,7 +1,51 @@
-import Main from './screens/Main';
+import { useEffect } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {
+  HomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  SplashScreen,
+} from './screens';
+
+import { useReducerWithThunks } from './hooks';
+import reducer, { initialState } from './store/reducer';
+import { getUser } from './store/thunks';
+import AppContext from './store/context';
+
+const { Navigator, Screen } = createNativeStackNavigator();
 
 const App = () => {
-  return <Main />;
+  const [state, dispatch] = useReducerWithThunks(reducer, initialState);
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, []);
+
+  if (state.loading) {
+    return <SplashScreen />;
+  }
+
+  return (
+    <AppContext.Provider value={[state, dispatch]}>
+      <NavigationContainer>
+        <Navigator>
+          {state.user ? (
+            <>
+              <Screen name='Home' component={HomeScreen} />
+            </>
+          ) : (
+            <>
+              <Screen name='Login' component={LoginScreen} />
+              <Screen name='Register' component={RegisterScreen} />
+            </>
+          )}
+        </Navigator>
+      </NavigationContainer>
+    </AppContext.Provider>
+  );
 };
 
 export default App;
