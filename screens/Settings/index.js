9abb5f38e17435184a6715920fca1173ff1d0c98
Robert Adams <adamsiii.robert@gmail.com>
2022-09-01 13:55:50 -0700

    Add logout button and functionality

--- screens/Settings/index.js
+++ screens/Settings/index.js
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native';
+import { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+
+import AppContext from '../../store/context';
+import { logout } from '../../store/thunks';
 
 import styles from './styles';
 
 const Settings = ({ navigation }) => {
+  const [, dispatch] = useContext(AppContext);
+
   return (
     <View style={styles.screen}>
       <Text>Settings</Text>
+      <Button title='Logout' onPress={() => dispatch(logout())} />
     </View>
   );
 };
