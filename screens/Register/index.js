1a191584fca5c19db74194f651283177b1e84867
Robert Adams <adamsiii.robert@gmail.com>
2022-09-01 13:55:30 -0700

    Add register functionality

--- screens/Register/index.js
+++ screens/Register/index.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { View, TextInput, Button } from 'react-native';
+
+import AppContext from '../../store/context';
+import { register } from '../../store/thunks';
 
 import styles from './styles';
 
-const Register = ({ navigation }) => {
+const Register = () => {
+  const [, dispatch] = useContext(AppContext);
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+
+  const submit = () => {
+    dispatch(register({ username, email, password }));
+  };
 
   return (
     <View>
@@ -16,7 +25,7 @@ const Register = ({ navigation }) => {
         onChangeText={setUsername}
       />
 
-      <TextInput placeholder='email' value={email} onChangeText={setUsername} />
+      <TextInput placeholder='email' value={email} onChangeText={setEmail} />
 
       <TextInput
         placeholder='password'
@@ -24,7 +33,7 @@ const Register = ({ navigation }) => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title='Register' onPress={() => console.log('REGISTER')} />
+      <Button title='Register' onPress={submit} />
     </View>
   );
 };
