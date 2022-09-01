10779c515c42134e878b94f311637646a19a50dd
Robert Adams <adamsiii.robert@gmail.com>
2022-09-01 13:44:17 -0700

    Move nav to Routes, add restoreToken to useEffect

--- App.js
+++ App.js
@@ -1,27 +1,18 @@
 import { useEffect } from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import {
-  HomeScreen,
-  LoginScreen,
-  RegisterScreen,
-  SplashScreen,
-} from './screens';
-
+import Routes from './routes';
+import { SplashScreen } from './screens';
 import { useReducerWithThunks } from './hooks';
 import reducer, { initialState } from './store/reducer';
 import { getUser } from './store/thunks';
 import AppContext from './store/context';
-
-const { Navigator, Screen } = createNativeStackNavigator();
+import { restoreToken } from './utils/helpers';
 
 const App = () => {
   const [state, dispatch] = useReducerWithThunks(reducer, initialState);
 
   useEffect(() => {
-    dispatch(getUser());
+    restoreToken().then(() => dispatch(getUser()));
   }, []);
 
   if (state.loading) {
@@ -30,20 +21,7 @@ const App = () => {
 
   return (
     <AppContext.Provider value={[state, dispatch]}>
-      <NavigationContainer>
-        <Navigator>
-          {state.user ? (
-            <>
-              <Screen name='Home' component={HomeScreen} />
-            </>
-          ) : (
-            <>
-              <Screen name='Login' component={LoginScreen} />
-              <Screen name='Register' component={RegisterScreen} />
-            </>
-          )}
-        </Navigator>
-      </NavigationContainer>
+      <Routes />
     </AppContext.Provider>
   );
 };
