767af9c9291c971624f338c55e0d2e12473b5d69
Robert Adams <adamsiii.robert@gmail.com>
2022-09-01 13:54:44 -0700

    Add login functionality

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,26 +1,36 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { View, TextInput, Button } from 'react-native';
+
+import AppContext from '../../store/context';
+import { login } from '../../store/thunks';
 
 import styles from './styles';
 
 const Login = ({ navigation }) => {
+  const [, dispatch] = useContext(AppContext);
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const submit = () => {
+    dispatch(login({ username, password }));
+  };
 
   return (
     <View>
       <TextInput
-        placeholder='Username'
+        placeholder='username'
         value={username}
         onChangeText={setUsername}
       />
       <TextInput
-        placeholder='Password'
+        placeholder='password'
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title='Login' onPress={() => console.log('LOG IN')} />
+      <Button title='Login' onPress={submit} />
+
       <Button
         title='Register'
         onPress={() => navigation.navigate('Register')}
