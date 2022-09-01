9da09b29b88cff86c11f85e6e0c9a87650bf7d91
Robert Adams <adamsiii.robert@gmail.com>
2022-08-31 20:59:06 -0700

    Add placeholder screen

---
+++ screens/Settings/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Settings = ({ navigation }) => {
+  return (
+    <View style={styles.screen}>
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default Settings;
