2b7ab0ca3443d5813e3986b0e4ff2ce9a7761275
Robert Adams <adamsiii.robert@gmail.com>
2022-08-31 20:56:29 -0700

    Add index to export screens

---
+++ screens/index.js
@@ -0,0 +1,15 @@
+import HomeScreen from './Home';
+import LoginScreen from './Login';
+import MainScreen from './Main';
+import RegisterScreen from './Register';
+import SettingsScreen from './Settings';
+import SplashScreen from './Spash';
+
+export {
+  HomeScreen,
+  LoginScreen,
+  MainScreen,
+  RegisterScreen,
+  SettingsScreen,
+  SplashScreen,
+};
