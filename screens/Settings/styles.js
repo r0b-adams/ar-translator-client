c8c1e03681394389d3fd4bf1f41d646142f8b3ef
Robert Adams <adamsiii.robert@gmail.com>
2022-09-02 19:05:03 -0700

    Prettier styles

--- screens/Settings/styles.js
+++ screens/Settings/styles.js
@@ -1,7 +1,11 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  screen: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 export default styles;
