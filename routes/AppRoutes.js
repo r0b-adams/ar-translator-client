ae81eb479cf9ed89a40eac426d38f96c1f4b6d68
Robert Adams <adamsiii.robert@gmail.com>
2022-09-02 19:02:39 -0700

    Add nav tab icons and focused styles

--- routes/AppRoutes.js
+++ routes/AppRoutes.js
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 import { HomeScreen, MainScreen, SettingsScreen } from '../screens';
 
@@ -6,10 +8,53 @@ const Tab = createBottomTabNavigator();
 
 const AppRoutes = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name='Home' component={HomeScreen} />
-      <Tab.Screen name='Main' component={MainScreen} />
-      <Tab.Screen name='Settings' component={SettingsScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerTitleAlign: 'center',
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <FontAwesome
+                name='home'
+                size={24}
+                color={focused ? '#108CC6' : 'black'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='Camera'
+        component={MainScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name='camera-sharp'
+              size={24}
+              color={focused ? '#108CC6' : 'black'}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Settings'
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name='settings'
+              size={24}
+              color={focused ? '#108CC6' : 'black'}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
