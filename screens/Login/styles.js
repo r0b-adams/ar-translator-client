abc5437c8870d7ddbee4ca00d398fe476e107e36
Robert Adams <adamsiii.robert@gmail.com>
2022-09-02 19:21:56 -0700

    Add style definitions

--- screens/Login/styles.js
+++ screens/Login/styles.js
@@ -1,7 +1,29 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  screen: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '90%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  form: {
+    backgroundColor: 'lightgray',
+    borderRadius: 10,
+  },
+  input: {
+    marginVertical: 10,
+    marginLeft: 10,
+    padding: 10,
+  },
+  register: {
+    marginTop: 25,
+    alignSelf: 'center',
+  },
+  link: {
+    color: '#108CC6',
+  },
 });
 
 export default styles;
