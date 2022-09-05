04399a1d2f3905c39cc6905288ab401ca187cfbc
Robert Adams <adamsiii.robert@gmail.com>
2022-09-04 22:22:13 -0700

    Prettier

--- store/reducer.js
+++ store/reducer.js
@@ -11,12 +11,16 @@ const reducer = (state, action) => {
   switch (action.type) {
     case ACTIONS.SET_LOADING:
       return { ...state, loading: action.payload };
+
     case ACTIONS.SET_USER:
       return { ...state, user: action.payload };
+
     case ACTIONS.SET_LANUAGES:
       return { ...state, languages: action.payload };
+
     case ACTIONS.SET_SELECTED_LANGUAGE:
       return { ...state, selectedLanguage: action.payload };
+
     default:
       return state;
   }
