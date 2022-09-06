4566fa28f902595f1e3df982c0b88d5ba2ae0729
Robert Adams <adamsiii.robert@gmail.com>
2022-09-05 23:02:37 -0700

    Move splash pg to nav stack

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 
 import Routes from './routes';
-import { SplashScreen } from './screens';
 import { useReducerWithThunks } from './hooks';
 import reducer, { initialState } from './store/reducer';
 import { getLanguages, getUser } from './store/thunks';
@@ -15,14 +14,9 @@ const App = () => {
     restoreToken().then(() => dispatch(getUser()));
   }, []);
 
-  // get list of supported languages on successful login
   useEffect(() => {
     if (state.user) dispatch(getLanguages());
   }, [state.user]);
-
-  if (state.loading) {
-    return <SplashScreen />;
-  }
 
   return (
     <AppContext.Provider value={[state, dispatch]}>
